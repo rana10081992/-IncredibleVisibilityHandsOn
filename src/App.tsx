67643628d4d7716b71f179e1c845b420f904fa92
rana10081992 <57004375+rana10081992@users.noreply.github.com>
2024-08-07 13:02:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <h1>Book List</h1>
-        kkjkkjkjk,jkj
       </header>
       <div className="TimeContainer">
         {data?.books.map(({ title, author }) => (
